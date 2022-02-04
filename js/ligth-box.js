65936d9272ced1f7d8324ad9c06d105326eaa831
Pablomu33 <97531853+Pablomu33@users.noreply.github.com>
2022-02-04 15:10:54 +0100

    Initial commit

---
+++ js/ligth-box.js
@@ -0,0 +1,29 @@
+const imagenes = document.querySelectorAll ('.img-galeria')
+const imagenesLigth = document.querySelector ('.agregar-imagen')
+const contenedorLigth = document.querySelector ('.imagen-ligth')
+
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener ('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+
+        
+
+    })
+})
+
+contenedorLigth.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLigth){
+        contenedorLigth.classList.toggle('show');
+        imagenesLigth.classList.toggle('showImage');
+    }
+})
+
+const aparecerImagen = (imagen)=>{
+
+    imagenesLigth.src = imagen;
+    contenedorLigth.classList.toggle('show');
+    imagenesLigth.classList.toggle('showImage');
+
+
+}
